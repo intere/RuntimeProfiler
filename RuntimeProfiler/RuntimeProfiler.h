ec7fb3b359af589670297555695ab72437f94f60
Eric Internicola <einternicola@silverchalice.com>
2015-09-06 07:17:58 -0600

    Initial Commit

---
+++ RuntimeProfiler/RuntimeProfiler.h
@@ -0,0 +1,19 @@
+//
+//  RuntimeProfiler.h
+//  RuntimeProfiler
+//
+//  Created by Eric Internicola on 9/6/15.
+//  Copyright (c) 2015 iColasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RuntimeProfiler.
+FOUNDATION_EXPORT double RuntimeProfilerVersionNumber;
+
+//! Project version string for RuntimeProfiler.
+FOUNDATION_EXPORT const unsigned char RuntimeProfilerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RuntimeProfiler/PublicHeader.h>
+
+
---
+++ RuntimeProfilerTests/RuntimeProfilerTests.m
@@ -0,0 +1,40 @@
+//
+//  RuntimeProfilerTests.m
+//  RuntimeProfilerTests
+//
+//  Created by Eric Internicola on 9/6/15.
+//  Copyright (c) 2015 iColasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RuntimeProfilerTests : XCTestCase
+
+@end
+
+@implementation RuntimeProfilerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
